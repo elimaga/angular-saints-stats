7d30f93a8e0dca83b5f13e0f82053858058f76aa
Elijah Magallanes <Elijah.Magallanes@hunterindustries.com>
2019-06-28 15:11:54 -0700

    Updated the main component to have the correct title;
    Updated the global styles;

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -16,16 +16,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'angular-saints-stats'`, () => {
+  it(`should have as title '2019 Spring Statistics'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('angular-saints-stats');
+    expect(app.title).toEqual('2019 Spring Statistics');
   });
 
   it('should render title in a h1 tag', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to angular-saints-stats!');
+    expect(compiled.querySelector('h1').textContent).toEqual('2019 Spring Statistics');
   });
 });
