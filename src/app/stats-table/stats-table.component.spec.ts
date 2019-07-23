a6d48912b8bd914ed05da62a2f014a82d582d415
Eli Magallanes <elimaga1996@gmail.com>
2019-07-22 22:17:07 -0700

    Calculating the stats for each player and ahndling error cases;

--- src/app/stats-table/stats-table.component.spec.ts
+++ src/app/stats-table/stats-table.component.spec.ts
@@ -1,9 +1,9 @@
-import {async, ComponentFixture, TestBed} from '@angular/core/testing';
-import {of} from 'rxjs';
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { of } from 'rxjs';
 
-import {StatsTableComponent} from './stats-table.component';
-import {HttpClientTestingModule} from '@angular/common/http/testing';
-import {StatsService} from '../stats.service';
+import { StatsTableComponent } from './stats-table.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { StatsService } from '../stats.service';
 
 describe('StatsTableComponent', () => {
   let component: StatsTableComponent;
@@ -11,13 +11,10 @@ describe('StatsTableComponent', () => {
   let statsService: StatsService;
 
   const fakeCategories = [
-    {id: 1, abbreviation: 'FC', name: 'Fake Category'},
-    {id: 2, abbreviation: 'CF', name: 'Category that is Fake'},
-    {id: 3, abbreviation: 'FY', name: 'Fakey Fake Category'}
+    { id: 2, abbreviation: 'FC', name: 'Fake Category' },
+    { id: 1, abbreviation: 'CF', name: 'Category that is Fake' },
+    { id: 3, abbreviation: 'FY', name: 'Fakey Fake Category' }
   ];
-
-  let fakePlayers;
-  let fakeStatistics;
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
@@ -37,16 +34,7 @@ describe('StatsTableComponent', () => {
 
     statsService = TestBed.get(StatsService);
 
-    fakePlayers = [
-      {id: 3, name: 'Fake Player'},
-      {id: 1, name: 'Player that is Fake'},
-      {id: 2, name: 'Fakey Fake Player'}
-    ];
-    fakeStatistics = [];
-
     spyOn(statsService, 'getStatsCategories').and.returnValue(of(fakeCategories));
-    spyOn(statsService, 'getPlayers').and.returnValue(of(fakePlayers));
-    spyOn(statsService, 'getStatisticsByPlayer').and.returnValue(of(fakeStatistics));
   }));
 
   it('should create', () => {
@@ -77,60 +65,71 @@ describe('StatsTableComponent', () => {
     });
 
     it('should set the stats categories of the component', () => {
+      const expectedCategoriesArray = [
+        { id: 1, abbreviation: 'CF', name: 'Category that is Fake' },
+        { id: 2, abbreviation: 'FC', name: 'Fake Category' },
+        { id: 3, abbreviation: 'FY', name: 'Fakey Fake Category' }
+      ];
+
       component.getStatsCategories();
 
-      expect(component.statsCategories).toEqual(fakeCategories);
+      expect(component.statsCategories).toEqual(expectedCategoriesArray);
     });
   });
 
   describe('getPlayersAndTheirStatistics', () => {
+    let fakePlayers;
+    let fakeStatistics;
+
+    beforeEach(() => {
+      fakePlayers = [
+        { id: 3, name: 'Fake Player' },
+        { id: 1, name: 'Player that is Fake' },
+        { id: 2, name: 'Fakey Fake Player' }
+      ];
+
+      fakeStatistics = [
+        { playerId: 6, categoryId: 1, value: 9 },
+        { playerId: 6, categoryId: 3, value: 14 },
+        { playerId: 6, categoryId: 4, value: 12 },
+        { playerId: 6, categoryId: 5, value: 1 },
+        { playerId: 6, categoryId: 6, value: 0 },
+        { playerId: 6, categoryId: 7, value: 0 },
+        { playerId: 6, categoryId: 8, value: 5 },
+        { playerId: 6, categoryId: 9, value: 10 },
+        { playerId: 6, categoryId: 10, value: 2 },
+        { playerId: 6, categoryId: 11, value: 8 },
+        { playerId: 6, categoryId: 12, value: 0 },
+        { playerId: 6, categoryId: 17, value: 0 },
+        { playerId: 6, categoryId: 18, value: 0 },
+        { playerId: 6, categoryId: 19, value: 0 },
+        { playerId: 6, categoryId: 20, value: 1 },
+        { playerId: 6, categoryId: 23, value: 0 },
+        { playerId: 6, categoryId: 24, value: 7 },
+        { playerId: 6, categoryId: 25, value: 6 },
+        { playerId: 6, categoryId: 27, value: 41 }
+      ];
+    });
+
     it('should use the stats service to get the players', () => {
+      spyOn(statsService, 'getPlayers').and.returnValue(of(fakePlayers));
+
       component.getPlayersAndTheirStatistics();
 
       expect(statsService.getPlayers).toHaveBeenCalled();
     });
 
-    it('should add the players and their numbers to the statistics array', () => {
-      const expectedStatisticsArray = [
-        {'#': 1, player: 'Player that is Fake'},
-        {'#': 2, player: 'Fakey Fake Player'},
-        {'#': 3, player: 'Fake Player'}
-      ];
-
-      component.getPlayersAndTheirStatistics();
-
-      expect(JSON.stringify(component.statistics)).toEqual(JSON.stringify(expectedStatisticsArray));
-    });
-
     it('should use the stats service to get the statistics for each player', () => {
+      spyOn(statsService, 'getPlayers').and.returnValue(of(fakePlayers));
+      spyOn(statsService, 'getStatisticsByPlayer').and.returnValue(of(fakeStatistics));
+
       component.getPlayersAndTheirStatistics();
 
       expect(statsService.getStatisticsByPlayer).toHaveBeenCalledTimes(fakePlayers.length);
     });
 
     it('should calculate all the statistics and add them to statistics array', () => {
-      fakePlayers = [{id: 6, name: 'Eli'}];
-      fakeStatistics = [
-        {playerId: 6, categoryId: 1, value: 9},
-        {playerId: 6, categoryId: 3, value: 14},
-        {playerId: 6, categoryId: 4, value: 12},
-        {playerId: 6, categoryId: 5, value: 1},
-        {playerId: 6, categoryId: 6, value: 0},
-        {playerId: 6, categoryId: 7, value: 0},
-        {playerId: 6, categoryId: 8, value: 5},
-        {playerId: 6, categoryId: 9, value: 10},
-        {playerId: 6, categoryId: 10, value: 2},
-        {playerId: 6, categoryId: 11, value: 8},
-        {playerId: 6, categoryId: 12, value: 0},
-        {playerId: 6, categoryId: 17, value: 0},
-        {playerId: 6, categoryId: 18, value: 0},
-        {playerId: 6, categoryId: 19, value: 0},
-        {playerId: 6, categoryId: 20, value: 1},
-        {playerId: 6, categoryId: 23, value: 0},
-        {playerId: 6, categoryId: 24, value: 7},
-        {playerId: 6, categoryId: 25, value: 6},
-        {playerId: 6, categoryId: 27, value: 41}
-      ];
+      fakePlayers = [{ id: 6, name: 'Eli' }];
       const expectedStatisticsArray = [
         {
           '#': 6,
@@ -147,10 +146,10 @@ describe('StatsTableComponent', () => {
           SO: 2,
           SB: 8,
           CS: 0,
-          AVG: 0.400,
-          OBP: 0.537,
-          SLG: 0.433,
-          OPS: 0.970,
+          AVG: '0.400',
+          OBP: '0.537',
+          SLG: '0.433',
+          OPS: '0.970',
           IBB: 0,
           HBP: 0,
           SAC: 0,
@@ -160,14 +159,80 @@ describe('StatsTableComponent', () => {
           GDP: 0,
           GO: 7,
           AO: 6,
-          GO_AO: 1.17,
+          GO_AO: '1.17',
           PA: 41
         }
       ];
+      spyOn(statsService, 'getPlayers').and.returnValue(of(fakePlayers));
+      spyOn(statsService, 'getStatisticsByPlayer').and.returnValue(of(fakeStatistics));
 
       component.getPlayersAndTheirStatistics();
 
-      expect(JSON.stringify(component.statistics)).toEqual(JSON.stringify(expectedStatisticsArray));
+      expect(component.statistics).toEqual(expectedStatisticsArray);
+    });
+
+    it('should handle error cases in the calculations', () => {
+      fakePlayers = [{ id: 6, name: 'Eli' }];
+      fakeStatistics = [
+        { playerId: 6, categoryId: 1, value: 9 },
+        { playerId: 6, categoryId: 3, value: 14 },
+        { playerId: 6, categoryId: 4, value: 12 },
+        { playerId: 6, categoryId: 5, value: 1 },
+        { playerId: 6, categoryId: 6, value: 0 },
+        { playerId: 6, categoryId: 7, value: 0 },
+        { playerId: 6, categoryId: 8, value: 5 },
+        { playerId: 6, categoryId: 9, value: 0 },
+        { playerId: 6, categoryId: 10, value: 2 },
+        { playerId: 6, categoryId: 11, value: 8 },
+        { playerId: 6, categoryId: 12, value: 0 },
+        { playerId: 6, categoryId: 17, value: 0 },
+        { playerId: 6, categoryId: 18, value: 0 },
+        { playerId: 6, categoryId: 19, value: 10 },
+        { playerId: 6, categoryId: 20, value: 0 },
+        { playerId: 6, categoryId: 23, value: 0 },
+        { playerId: 6, categoryId: 24, value: 7 },
+        { playerId: 6, categoryId: 25, value: 0 },
+        { playerId: 6, categoryId: 27, value: 10 }
+      ];
+      const expectedStatisticsArray = [
+        {
+          '#': 6,
+          player: 'Eli',
+          G: 9,
+          AB: 0,
+          R: 14,
+          H: 12,
+          '2B': 1,
+          '3B': 0,
+          HR: 0,
+          RBI: 5,
+          BB: 0,
+          SO: 2,
+          SB: 8,
+          CS: 0,
+          AVG: '.---',
+          OBP: '.---',
+          SLG: '.---',
+          OPS: '.---',
+          IBB: 0,
+          HBP: 0,
+          SAC: 10,
+          SF: 0,
+          TB: 13,
+          XBH: 1,
+          GDP: 0,
+          GO: 7,
+          AO: 0,
+          GO_AO: '.--',
+          PA: 10
+        }
+      ];
+      spyOn(statsService, 'getPlayers').and.returnValue(of(fakePlayers));
+      spyOn(statsService, 'getStatisticsByPlayer').and.returnValue(of(fakeStatistics));
+
+      component.getPlayersAndTheirStatistics();
+
+      expect(component.statistics).toEqual(expectedStatisticsArray);
     });
   });
 });
--- src/app/stats-table/stats-table.component.ts
+++ src/app/stats-table/stats-table.component.ts
@@ -1,8 +1,8 @@
-import {Component, OnInit} from '@angular/core';
-import {StatsService} from '../stats.service';
-import {StatsCategory} from '../objectClasses/statsCategory';
-import {Player} from '../objectClasses/player';
-import {Statistic} from '../objectClasses/statistic';
+import { Component, OnInit } from '@angular/core';
+import { StatsService } from '../stats.service';
+import { StatsCategory } from '../objectClasses/statsCategory';
+import { Player } from '../objectClasses/player';
+import { Statistic } from '../objectClasses/statistic';
 
 @Component({
   selector: 'app-stats-table',
@@ -12,7 +12,7 @@ import {Statistic} from '../objectClasses/statistic';
 export class StatsTableComponent implements OnInit {
   statsCategories: StatsCategory[];
   players: Player[];
-  statistics;
+  statistics = [];
 
   constructor(private statsService: StatsService) {
   }
@@ -23,12 +23,7 @@ export class StatsTableComponent implements OnInit {
 
   getStatsCategories(): void {
     this.statsService.getStatsCategories()
-      .subscribe(categories => this.statsCategories = categories);
-  }
-
-  private getPlayers(callback): void {
-    this.statsService.getPlayers()
-      .subscribe(players => callback(players));
+      .subscribe(categories => this.sortCategoriesAndSave(categories));
   }
 
   getPlayersAndTheirStatistics(): void {
@@ -37,15 +32,6 @@ export class StatsTableComponent implements OnInit {
         return player1.id - player2.id;
       });
 
-      this.statistics = players.map(player => {
-        return {
-          '#': player.id,
-          player: player.name
-        };
-      });
-
-      const statisticsFromDB: Statistic[][] = [];
-
       players.forEach((player) => {
         this.statsService.getStatisticsByPlayer(player.id)
           .subscribe(statistics => {
@@ -53,13 +39,65 @@ export class StatsTableComponent implements OnInit {
               return stat1.categoryId - stat2.categoryId;
             });
 
-            statisticsFromDB.push(statistics);
+            const errorTwoDecimals = '.--';
+            const errorThreeDecimals = '.---';
+
+            const plateAppearances = statistics[18].value;
+            const walks = statistics[7].value + statistics[12].value;
+            const atBats = plateAppearances - walks - statistics[11].value - statistics[13].value - statistics[14].value;
+            const totalBases = (statistics[2].value - statistics[3].value - statistics[4].value - statistics[5].value) +
+              (2 * statistics[3].value) + (3 * statistics[4].value) + (4 * statistics[5].value);
+            const sluggingPercentage = totalBases / atBats;       
+            const plateAppearancesNonSac = (atBats + walks + statistics[14].value);
+            const onBasePercentage = (statistics[2].value + walks) / plateAppearancesNonSac;
+
+
+            this.statistics.push({
+              '#': player.id,
+              player: player.name,
+              G: statistics[0].value,
+              AB: atBats,
+              R: statistics[1].value,
+              H: statistics[2].value,
+              '2B': statistics[3].value,
+              '3B': statistics[4].value,
+              HR: statistics[5].value,
+              RBI: statistics[6].value,
+              BB: statistics[7].value,
+              SO: statistics[8].value,
+              SB: statistics[9].value,
+              CS: statistics[10].value,
+              AVG: atBats ? (statistics[2].value / atBats).toFixed(3) : errorThreeDecimals,
+              OBP: plateAppearancesNonSac ? onBasePercentage.toFixed(3) : errorThreeDecimals,
+              SLG: atBats ? sluggingPercentage.toFixed(3) : errorThreeDecimals,
+              OPS: atBats && plateAppearancesNonSac ? (onBasePercentage + sluggingPercentage).toFixed(3) : errorThreeDecimals,
+              IBB: statistics[11].value,
+              HBP: statistics[12].value,
+              SAC: statistics[13].value,
+              SF: statistics[14].value,
+              TB: totalBases,
+              XBH: statistics[3].value + statistics[4].value + statistics[5].value,
+              GDP: statistics[15].value,
+              GO: statistics[16].value,
+              AO: statistics[17].value,
+              GO_AO: statistics[17].value ? (statistics[16].value / statistics[17].value).toFixed(2) : errorTwoDecimals,
+              PA: plateAppearances
+            });
           });
       });
-
-      const calculatedStats = statisticsFromDB.map(statistics => {
-        return {};
-      });
     });
+  }
+
+  private sortCategoriesAndSave(categories) {
+    categories.sort((category1, category2) => {
+      return category1.id - category2.id;
+    });
+
+    this.statsCategories = categories;
+  }
+
+  private getPlayers(callback): void {
+    this.statsService.getPlayers()
+      .subscribe(players => callback(players));
   }
 }
