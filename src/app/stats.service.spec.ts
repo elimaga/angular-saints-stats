7aee4f0224e4052349a78ee7b01c396eb0fa9151
Elijah Magallanes <Elijah.Magallanes@hunterindustries.com>
2019-08-29 16:19:43 -0700

    Updated stats service to have only the functions we need;

--- src/app/stats.service.spec.ts
+++ src/app/stats.service.spec.ts
@@ -64,40 +64,6 @@ describe('StatsService', () => {
     });
   });
 
-  describe('Players', () => {
-    let dataCallback;
-    let errorCallback;
-    let testObserver;
-
-    beforeEach(() => {
-      dataCallback = sinon.spy();
-      errorCallback = sinon.spy();
-      testObserver = {
-        next: dataCallback,
-        error: errorCallback
-      };
-    });
-
-    it('should make an http request to get the players', () => {
-      const fakePlayers = [
-        {id: 1, name: 'Fake Player'},
-        {id: 2, name: 'Player that is Fake'}
-      ];
-
-      service.getPlayers().subscribe(testObserver);
-
-      const req = httpMock.expectOne('api/players');
-      expect(req.request.method).toBe('GET');
-      req.flush(fakePlayers);
-
-      const players = dataCallback.args[0][0];
-      expect(players.length).toBe(fakePlayers.length);
-      expect(players).toEqual(fakePlayers);
-
-      expect(errorCallback.callCount).toEqual(0);
-    });
-  });
-
   describe('Statistics', () => {
     let dataCallback;
     let errorCallback;
@@ -112,12 +78,10 @@ describe('StatsService', () => {
       };
     });
 
-    it('should make an http request to get the statistics by the playerId', () => {
-      const playerId = 2;
+    it('should make an http request to get all of the statistics for all players', () => {
+      service.getStatistics().subscribe(testObserver);
 
-      service.getStatisticsByPlayer(playerId).subscribe(testObserver);
-
-      const req = httpMock.expectOne(`api/statistics?playerId=^${playerId}$`);
+      const req = httpMock.expectOne(`https://pksh7vt4mh.execute-api.us-west-1.amazonaws.com/test/statistics`);
       expect(req.request.method).toBe('GET');
     });
   });
--- src/app/stats.service.ts
+++ src/app/stats.service.ts
@@ -3,16 +3,13 @@ import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 
 import { StatsCategory } from './objectClasses/statsCategory';
-import { Player } from './objectClasses/player';
-import { Statistic } from './objectClasses/statistic';
 
 @Injectable({
   providedIn: 'root'
 })
 export class StatsService {
   private statsCategoriesUrl = 'https://pksh7vt4mh.execute-api.us-west-1.amazonaws.com/test/statscategories';
-  private playersUrl = 'api/players';
-  private statisticsUrl = 'api/statistics';
+  private statisticsUrl = 'https://pksh7vt4mh.execute-api.us-west-1.amazonaws.com/test/statistics';
 
   constructor(private http: HttpClient) { }
 
@@ -20,11 +17,7 @@ export class StatsService {
     return this.http.get<StatsCategory[]>(this.statsCategoriesUrl);
   }
 
-  getPlayers(): Observable<Player[]> {
-    return this.http.get<Player[]>(this.playersUrl);
-  }
-
-  getStatisticsByPlayer(playerId: number): Observable<Statistic[]> {
-    return this.http.get<Statistic[]>(`${this.statisticsUrl}?playerId=^${playerId}$`);
+  getStatistics(): Observable<any[]> {
+    return this.http.get<any[]>(this.statisticsUrl);
   }
 }
